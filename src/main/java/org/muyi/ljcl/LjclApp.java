c2c764a1f5f100f33be5acf6466d22bd535d23e0
muyijt0 <muyijt0@163.com>
2026-01-15 18:41:03 +0800

    引入spring boot和spring cloud依赖

---
+++ src/main/java/org/muyi/ljcl/LjclApp.java
@@ -0,0 +1,11 @@
+package org.muyi.ljcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjclApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LjclApp.class, args);
+    }
+}
